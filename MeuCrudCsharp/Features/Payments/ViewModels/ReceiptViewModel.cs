8c61237646ce4c69da4c4db93a45b50daed88efa
gregrymqt <lucasvicentedesouza021@gmail.com>
2025-07-26 22:21:17 -0300

    ajuste no modelo referente ao recibo

--- MeuCrudCsharp/Features/Payments/ViewModels/ReceiptViewModel.cs
+++ MeuCrudCsharp/Features/Payments/ViewModels/ReceiptViewModel.cs
@@ -1,21 +1,40 @@
-﻿namespace MeuCrudCsharp.Features.Payments.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MeuCrudCsharp.Features.Payments.ViewModels
 {
     public class ReceiptViewModel
     {
+        [Required]
+        [MaxLength(50)]
         public string CompanyName { get; set; }
 
+        [Required]
+        [MaxLength(21)]
         public string CompanyCnpj { get; set; }
 
+        [Required]
+        [MaxLength(30)]
         public string PaymentId { get; set; }
 
-        public System.DateTime PaymentDate { get; set; }
+        [Required]
+        public DateTime PaymentDate { get; set; }
 
+        [Required]
+        [MaxLength(50)]
         public string CustomerName { get; set; }
 
+        [Required]
+        [MaxLength(15)]
         public string CustomerCpf { get; set; }
 
+        [Required]
         public decimal Amount { get; set; }
 
+        [Required]
+        [MaxLength(25)]
         public string PaymentMethod { get; set; }
+
+        [Required]
+        public int LastFourDigits { get; set; }
     }
 }
