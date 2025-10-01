60e4bc540213a90507094f8f31b97ab784b62aff
gregrymqt <lucasvicentedesouza021@gmail.com>
2025-10-01 11:08:58 -0300

    finalização do css do perfil e do vídeos, pois estava incorreto

--- MeuCrudCsharp/AppSettings.cs
+++ MeuCrudCsharp/AppSettings.cs
@@ -1,49 +1,49 @@
 public class GeneralSettings
 {
     public const string SectionName = "General";
-    public string BaseUrl { get; set; }
+    public string? BaseUrl { get; set; }
 }
 
 public class MercadoPagoSettings
 {
     public const string SectionName = "MercadoPago";
 
-    public string PublicKey { get; set; }
-    public string AccessToken { get; set; }
-    public string WebhookSecret { get; set; }
-    public string Defaultdescription { get; set; }
-    public Dictionary<string, PlanDetail> Plans { get; set; }
+    public string? PublicKey { get; set; }
+    public string? AccessToken { get; set; }
+    public string? WebhookSecret { get; set; }
+    public string? Defaultdescription { get; set; }
+    public Dictionary<string, PlanDetail>? Plans { get; set; }
 }
 
 public class PlanConfiguration
 {
-    public PlanDetail Mensal { get; set; }
-    public PlanDetail Anual { get; set; }
+    public PlanDetail? Mensal { get; set; }
+    public PlanDetail? Anual { get; set; }
 }
 
 public class PlanDetail
 {
-    public string Id { get; set; }
+    public string? Id { get; set; }
     public decimal Price { get; set; }
 }
 
 public class SendGridSettings
 {
     public const string SectionName = "SendGrid";
-    public string ApiKey { get; set; }
-    public string FromEmail { get; set; }
-    public string FromName { get; set; }
+    public string? ApiKey { get; set; }
+    public string? FromEmail { get; set; }
+    public string? FromName { get; set; }
 }
 
 public class JwtSettings
 {
-    public string Key { get; set; }
+    public string? Key { get; set; }
 }
 
 public class GoogleSettings
 {
     public const string SectionName = "Google";
     
-    public string ClientId { get; set; }
-    public string ClientSecret { get; set; }
+    public string? ClientId { get; set; }
+    public string? ClientSecret { get; set; }
 }
